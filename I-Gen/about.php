a8470c22777e3e3796f99c55e1a31c007d840a30
Pragya-Adhikary <pragyaa633@gmail.com>
2025-01-10 19:13:36 +0545

    ggllaaffddrr

---
+++ I-Gen/about.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About - EduMap Nepal</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body class="about">
+  <nav>
+    <div class="nav-button">
+       <a href="home.php" class="nbutton">Home</a>
+       <a href="about.php" class="nbutton">About</a>
+       <a href="map.php" class="nbutton">Map</a>
+       <a href="contribute.php" class="nbutton">Contribute</a>
+       <a href="contact.php" class="nbutton">Contact</a>
+    </div>
+        <div class="auth-button">
+            <a href="login.html" class="button">Login</a>
+        </div>
+    </nav>
+    <section class="container">
+        <h2 style="color:  rgb(162, 160, 160);">About EduMap Nepal</h2>
+        <p style="color: rgb(162, 160, 160)">At EduMap Nepal, we strive to bridge the gap in education by bringing visibility to the needs of schools in Nepal's
+         most remote areas. Our mission is to ensure equal access to education for all, fostering development and empowering communities.</p></section>
+    <div class="card-container">
+        <div class="card">
+          <h3 style="color: #161e28">Who Are We?</h3>
+          <p style="color: #2f3f54">
+            NepalEdu Map is a dedicated platform designed to bring visibility to the education sector in Nepal by identifying and 
+            mapping unreached schools across the country. We believe education is a fundamental right, and our mission is to ensure
+             that no school or community is left behind.
+          </p>
+        </div>
+        <div class="card">
+          <h3 style="color: #161e28">Why EduMap Nepal?</h3>
+          <p style="color: #2f3f54"> Despite Nepal's significant strides in education, thousands of schools in remote areas still lack access to basic resources 
+            such as qualified teachers, infrastructure, and learning materials. Many of these schools remain undocumented, making it 
+            challenging to allocate aid and assistance effectively.</p>
+            
+          </p>
+        </div>
+        <div class="card">
+          <h3 style="color: #161e28">What We Do?</h3>
+          <p style="color: #2f3f54">  We use geographic data and on-the-ground information to map schools in the most remote and hard-to-reach areas of Nepal. 
+            Each school is pinned on our digital map, providing critical data like location, accessibility, facilities, and needs.
+            We collect and analyze data to identify gaps in educational access and resources.</p>
+          
+        </div>
+      </div>
+      
+    <footer>
+        <p>&copy; 2025 EduMap Nepal. All rights reserved.</p>
+    </footer>
+    <script src="main.js"></script>
+</body>
+</html>
